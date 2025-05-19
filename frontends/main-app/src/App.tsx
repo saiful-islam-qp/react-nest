f6b90a9f2f129859f5f20dec92f6845b64814d90
anish bhanwala <anish.bhanwala@questionpro.com>
2025-05-19 17:39:50 +0530

    - setup msw - setup wick-ui-lib - setup react-query

--- frontends/main-app/src/App.tsx
+++ frontends/main-app/src/App.tsx
@@ -1,13 +1,71 @@
-import {useState} from 'react'
+import {WuPrimaryNavbar} from '@npm-questionpro/wick-ui-lib'
 import './App.css'
 import {TodoListScreen} from './screens/TodoListScreen'
+import {API_BASE_URL} from './constants/appConstants'
+import type {IServerResponse} from './types/IServerResponse'
+import type {IUser} from './types/IUser'
+import {useQuery, type UseQueryResult} from '@tanstack/react-query'
+
+const fetchUser = async (): Promise<IServerResponse<IUser>> => {
+  return fetch(`${API_BASE_URL}user`, {
+    method: 'GET',
+    headers: {
+      // eslint-disable-next-line @typescript-eslint/naming-convention
+      'Content-Type': 'application/json',
+    },
+  }).then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok')
+    }
+    return response.json() as Promise<IServerResponse<IUser>>
+  })
+}
+
+const useUserApi = (): UseQueryResult<IServerResponse<IUser>, Error> => {
+  return useQuery({
+    queryKey: ['user'],
+    queryFn: fetchUser,
+  })
+}
 
 export const App: React.FC = () => {
-  const [count, setCount] = useState(0)
+  const {data, error, isLoading} = useUserApi()
+
+  if (isLoading) {
+    return <div>Loading...</div>
+  }
+
+  if (error) {
+    console.error('Error fetching user:', error)
+    return <div>Error: {error?.message || 'Something went wrong'}</div>
+  }
+  const user = data?.data
+  if (!user) {
+    throw new Error('User not found')
+  }
 
   return (
     <>
-      <TodoListScreen />
+      <div>
+        <WuPrimaryNavbar
+          Links={[
+            <a key="home" href="#" className="active">
+              Home
+            </a>,
+            <a key="about" href="#">
+              About
+            </a>,
+            <a key="services" href="#">
+              Services
+            </a>,
+            <a key="contact" href="#">
+              Contact
+            </a>,
+          ]}
+        />
+        <TodoListScreen />
+      </div>
+      <h1>{user?.email}</h1>
     </>
   )
 }
---
+++ frontends/main-app/src/providers/AppProviders.tsx
@@ -0,0 +1,22 @@
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
+import React from 'react'
+
+export const AppProviders: React.FC<React.PropsWithChildren> = ({children}) => {
+  // Create a client using ref to avoid re-creating it on every render
+  const queryClientRef = React.useRef<QueryClient | null>(null)
+  if (!queryClientRef.current) {
+    queryClientRef.current = new QueryClient({
+      defaultOptions: {
+        queries: {
+          retry: false,
+        },
+      },
+    })
+  }
+
+  return (
+    <QueryClientProvider client={queryClientRef.current}>
+      {children}
+    </QueryClientProvider>
+  )
+}
