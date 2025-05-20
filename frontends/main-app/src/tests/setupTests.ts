2e416e6360007128f1fc5cf0c1e0a099725f2343
anish bhanwala <anish.bhanwala@questionpro.com>
2025-05-20 17:03:13 +0530

    testing setup

---
+++ frontends/main-app/src/tests/setupTests.ts
@@ -0,0 +1,26 @@
+import {afterEach} from 'vitest'
+import {cleanup} from '@testing-library/react'
+import * as ResizeObserverModule from 'resize-observer-polyfill'
+
+import '@testing-library/dom'
+import {mswTestServer} from '../msw/mswTestServer'
+
+beforeAll(() => {
+  // https://stackoverflow.com/a/72543616/8596140
+  // This is required by wick-ui-lib WuModal component
+  window.ResizeObserver = ResizeObserverModule.default
+  HTMLElement.prototype.scrollIntoView = vi.fn()
+  mswTestServer.listen()
+})
+
+beforeEach(() => {})
+
+afterEach(() => {
+  // Reset all request handlers
+  mswTestServer.resetHandlers()
+  cleanup()
+})
+
+afterAll(() => {
+  mswTestServer.close()
+})
